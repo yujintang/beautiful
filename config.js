98a8510fc4230d6be277b1b5fc2a321b73ab2a45
余金堂 <shanquan54@163.com>
2017-07-19 14:28:05 +0800

    ✨add

---
+++ config.js
@@ -0,0 +1,15 @@
+/**
+ * Created by yujintang on 2017/7/18.
+ */
+'use strict';
+ 
+const host = 'http://127.0.0.1:12121';
+
+const config = {};
+
+config.Url = {
+  loginUrl: host + '/auth/miniProgram',
+};
+
+module.exports = config;
+
